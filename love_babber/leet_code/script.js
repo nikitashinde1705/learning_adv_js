6a1c0b6e01cc03dfe30a0ef075611223bea9e305
Nikita Shinde <nikitashinde1705@gmail.com>
2025-11-11 18:30:04 +0530

    today i learn how to data fetch

---
+++ love_babber/leet_code/script.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", function(){
+    
+    const searchButton = document.getElementById("search-btn");
+    const usernameInput = document.getElementById("user-input");
+    const statsContainer = document.querySelector(".stats-container");
+    const easyProgressCircle = document.querySelector(".easy-progress");
+    const mediumProgressCircle = document.querySelector(".medium-progress");
+    const hardProgressCircle = document.querySelector(".hard-progress");
+    const easyLabel = document.getElementById("easy-label");
+    const mediumLabel = document.getElementById("medium-label");
+    const hardLabel = document.getElementById("hard-label");
+    const cardStatsContainer = document.querySelector(".stats-cards");
+
+
+    //return true or false based on a regular explression
+    function validateUsername(username) {
+        if(username.trim() === ""){
+            alert("username should not be empty");
+            return false;
+        }
+
+        const regex = /^[a-zA-Z0-9_-]{1,15}$/;
+        
+        const isMatching = regex.test(username);
+
+        if(!isMatching) {
+            alert("Invalid username");
+        }
+        return isMatching;
+    }
+
+    async function fetchUserDetails(username){
+        const url = `https://leetcode-stats-api.herokuapp.com/${username}`
+        try{
+            searchButton.textContent = "Searching...";
+            searchButton.disabled = true;
+
+
+            const response = await fetch(url);
+            if(!resoponse.ok){
+                throw new Error("Unable to fetch the User details");
+            }
+            const data = await response.json();
+            console.log("Logging data: ",data);
+        }
+        catch(error) {
+            statsContainer.innerHTML = `<p>No data Found`
+        }
+        finally{
+            searchButton.textContent = "Search";
+            searchButton.disabled = false;
+
+        }
+    }
+
+    searchButton.addEventListener('click', function(){
+        const username = usernameInput.value;
+        console.log("loggin username: ",username);
+        if(validateUsername(username)){
+            fetchUserDetails(username);
+        }
+    })
+
+
+})
